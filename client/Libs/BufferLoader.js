2ec98bedff062366c97e189c36ecae009bd780d3
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2020-05-23 00:45:17 +0530

    Deploy

---
+++ client/Libs/BufferLoader.js
@@ -0,0 +1,38 @@
+﻿function BufferLoader(context, urlList, callback) {
+    this.context = context;
+    this.urlList = urlList;
+    this.onload = callback;
+    this.bufferList = new Array();
+    this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function (url, index) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    var loader = this;
+
+    request.onload = function () {
+        loader.context.decodeAudioData(request.response, function (buffer) {
+            if (!buffer) {
+                console.error("Error Decoding File Data : " + url);
+                return;
+            }
+            loader.bufferList[index] = buffer;
+            if (++loader.loadCount == loader.urlList.length)
+                loader.onload(loader.bufferList);
+        });
+    };
+
+    request.onerror = function () {
+        console.error("BufferLoader: XHR Error");
+    };
+
+    request.send();
+};
+
+BufferLoader.prototype.load = function () {
+    for (var i = 0; i < this.urlList.length; ++i)
+        this.loadBuffer(this.urlList[i], i);
+};
