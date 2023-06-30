af469c1896bcdc0a8f571b76b6d15d327f703b90
saurabh-prosoft <saurabhbhagat98die@gmail.com>
2023-06-30 23:44:25 +0530

    error page

--- src/containers/Error/Error.jsx
+++ src/containers/Error/Error.jsx
@@ -1,7 +1,22 @@
+import Icon from "@/components/common/Icon/Icon";
 import styles from "./Error.module.css";
+import Button from "@/components/common/Button/Button";
 
 const Error = ({}) => {
-  return <main></main>;
+  return (
+    <main className={styles.error}>
+      <section className={styles.title}>
+        <Icon size={3.5} name="warninig" accent="dark" className={styles.icon} />
+        <h1>Error !</h1>
+        <h3>Please try reloading the page</h3>
+      </section>
+      <section className={styles.content}>
+        <Button icon="bug" left accent="dark">
+          Report an issue
+        </Button>
+      </section>
+    </main>
+  );
 };
 
 export default Error;
